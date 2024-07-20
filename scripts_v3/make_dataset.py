632702df1dd9de25836c7c88116ed5b204203af1
Andrea <wan.andrea@gmail.com>
2024-07-20 15:48:37 -0700

    Ready for pickles

---
+++ scripts_v3/make_dataset.py
@@ -0,0 +1,29 @@
+from crop_imgs import *
+import glob
+
+parent_folder = "C:\\Users\\andre\\Documents\\lineweights\\processed_data"
+# a list containing dictionaries for each drawing
+labels_imgs = []
+
+# Make sure the pkl files are read in numerical order 
+filenames = sorted(glob.glob(os.path.join(parent_folder, '*.pkl')), key=lambda x: int(os.path.basename(x).split(".")[0]))
+
+for filename in filenames:
+    print(f"Processing file: {filename}")
+    draw_num = os.path.basename(filename)[:-4]
+    data = pklToLst(filename)
+    normal_img = parent_folder + "\\" + "normal" + "_" + str(draw_num) + ".jpg"
+    labels_imgs.append(makeCroppedImgs(normal_img, 2250, 330, data, draw_num))
+
+
+"""
+base_path = "C:\\Users\\andre\\Documents\\lineweights\\data" 
+
+for folder in glob.glob(os.path.join(base_path, "*")):
+    folder_name = os.path.basename(folder)
+    num = int(folder_name)  # remove leading zeroes
+    image_path = os.path.join(folder, "normal.jpg")
+    new_image_path = os.path.join(folder, f"normal_{num}.jpg")
+    os.rename(image_path, new_image_path)
+    print(f"Renamed {image_path} to {new_image_path}")
+"""
